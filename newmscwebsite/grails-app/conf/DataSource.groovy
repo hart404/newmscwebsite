03395dd8ce8dd2f857cc1d6af2bfed02c6984664
Philip Hartley <hart404@gmail.com>
2013-11-04 14:21:33 -0700

    NOJIRA Added backup datasource

--- newmscwebsite/grails-app/conf/DataSource.groovy
+++ newmscwebsite/grails-app/conf/DataSource.groovy
@@ -47,6 +47,25 @@ environments {
 			}
 		}
 	}
+	backup {
+		dataSource {
+			pooled = true
+			driverClassName = "com.mysql.jdbc.Driver"
+			url = "jdbc:mysql://mscmscbackup.clchcirmqiuh.us-west-1.rds.amazonaws.com/mscmsc"
+			username = "root"
+			password = "uni-dev01"
+			dialect = org.hibernate.dialect.MySQLDialect
+			properties {
+				validationQuery = "SELECT 1"
+				testOnBorrow = true
+				testOnReturn = true
+				testWhileIdle = true
+				timeBetweenEvictionRunsMillis = 1000 * 60 * 30
+				numTestsPerEvictionRun = 3
+				minEvictableIdleTimeMillis = 1000 * 60 * 30
+			}
+		}
+	}
 	phil {
 		dataSource {
 			pooled = true
